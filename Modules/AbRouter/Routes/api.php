4e8a8aa560b7ba1049c3868c2cc19b076da51203
Yatsenko <webfay1@gmail.com>
2021-10-14 19:56:35 +0300

    Fix short token

--- Modules/AbRouter/Routes/api.php
+++ Modules/AbRouter/Routes/api.php
@@ -1,10 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use Modules\Auth\Http\Middleware\SimpleTokenMiddleware;
 
 Route::get('/v1/experiment/run', 'ExperimentsController@runSimple');
 
-Route::middleware([SimpleTokenMiddleware::class, 'api', 'auth:api'])->group(function () {
+Route::middleware(['api', 'auth:api'])->group(function () {
     Route::post('/v1/experiments/', 'ExperimentsController@createOrUpdate');
     Route::patch('/v1/experiments/{id}', 'ExperimentsController@createOrUpdate');
     Route::get('/v1/experiments', 'ExperimentsController@index');
