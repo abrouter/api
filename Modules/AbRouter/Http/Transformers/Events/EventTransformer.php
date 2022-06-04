70fb6c4570c695e1f8278fc7d4c4972845513130
Oleksii Yatsenko <yatsenkolesh@gmail.com>
2022-06-04 14:17:10 +0100

    Remove user id from statistic request

--- Modules/AbRouter/Http/Transformers/Events/EventTransformer.php
+++ Modules/AbRouter/Http/Transformers/Events/EventTransformer.php
@@ -5,6 +5,7 @@ namespace Modules\AbRouter\Http\Transformers\Events;
 
 use Illuminate\Http\Request;
 use Modules\AbRouter\Services\Events\DTO\EventDTO;
+use Modules\Auth\Exposable\AuthDecorator;
 use Modules\Core\EntityId\EntityEncoder;
 use Modules\Core\EntityId\IdTrait;
 
@@ -14,16 +15,19 @@ class EventTransformer
      * @var EntityEncoder
      */
     private $encoder;
-    
-    public function __construct(EntityEncoder $encoder)
+
+    private AuthDecorator $authDecorator;
+
+    public function __construct(EntityEncoder $encoder, AuthDecorator $authDecorator)
     {
+        $this->authDecorator = $authDecorator;
         $this->encoder = $encoder;
     }
     
     public function transform(Request $request): EventDTO
     {
         return new EventDTO(
-            $this->encoder->decode($request->input('data.relationships.owner.data.id'), 'users'),
+            $this->authDecorator->get()->getId(),
             $request->input('data.attributes.temporary_user_id'),
             $request->input('data.attributes.user_id'),
             $request->input('data.attributes.event'),
