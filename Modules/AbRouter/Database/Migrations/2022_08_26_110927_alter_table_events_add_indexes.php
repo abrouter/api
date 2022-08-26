13430937ea20aeff5398d1ddc7a3193cfae904d6
Alex Yatsenko <yatsenkolesh@gmail.com>
2022-08-26 12:31:54 +0100

    Add index composite for events.user_id + events.created_at (#33)

--- Modules/AbRouter/Database/Migrations/2022_08_26_110927_alter_table_events_add_indexes.php
+++ Modules/AbRouter/Database/Migrations/2022_08_26_110927_alter_table_events_add_indexes.php
@@ -15,7 +15,7 @@ class AlterTableEventsAddIndexes extends Migration
     {
         Schema::table('events', function (Blueprint $table) {
             $table->dropIndex('events_user_id_index');
-            $table->index(['user_id', 'created_at']);
+            $table->index(['owner', 'created_at']);
         });
     }
 
