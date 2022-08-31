b2ee1aa806c72e7a07549c866ff3f859154a160e
Vlad <82599515+Omelya@users.noreply.github.com>
2022-09-01 00:02:15 +0300

    DEV-160 Restrict creating experiment/feature flag with same uid if its already exists (#34)

--- Modules/AbRouter/Http/Requests/ExperimentRequest.php
+++ Modules/AbRouter/Http/Requests/ExperimentRequest.php
@@ -6,6 +6,7 @@ namespace Modules\AbRouter\Http\Requests;
 use Illuminate\Validation\Rule;
 use Modules\Core\Http\Requests\FormRequest;
 use Modules\Core\Rules\SumPercent;
+use Modules\Core\EntityId\EntityEncoder;
 
 class ExperimentRequest extends FormRequest
 {
@@ -16,18 +17,32 @@ class ExperimentRequest extends FormRequest
 
     public function rules(): array
     {
+        if ($this->input('data.id')) {
+            $id = (new EntityEncoder())->decode($this->input('data.id'), 'experiments');
+        }
+
         return [
             'data.type' => [
                 'required',
                 Rule::in('experiments')
             ],
             'data.attributes.name' => 'required|string',
-            'data.attributes.alias' => 'required|string',
+            'data.attributes.alias' => ['required', 'string',
+                Rule::unique('experiments', 'alias')
+                    ->ignore($id ?? null),
+            ],
             'included' => ['array', 'min:1', new SumPercent],
             'included.*.type' => ['required', Rule::in('experiment_branches')],
             'included.*.attributes.name' => 'required|string|distinct',
             'included.*.attributes.percent' => 'required|integer|between:0,100',
             'included.*.attributes.uid' => 'required|string|distinct'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'unique' => 'Experiment uid already exist.',
         ];
     }
 }
--- Modules/AbRouter/Http/Requests/FeatureToggleCreateRequest.php
+++ Modules/AbRouter/Http/Requests/FeatureToggleCreateRequest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Modules\AbRouter\Http\Requests;
 
 use Illuminate\Validation\Rule;
+use Modules\Core\EntityId\EntityEncoder;
 use Modules\Core\Http\Requests\FormRequest;
 use Modules\Core\Rules\SumPercent;
 
@@ -16,13 +17,20 @@ class FeatureToggleCreateRequest extends FormRequest
 
     public function rules(): array
     {
+        if ($this->input('data.id')) {
+            $id = (new EntityEncoder())->decode($this->input('data.id'), 'experiments');
+        }
+
         return [
             'data.type' => [
                 'required',
                 Rule::in('feature-toggles')
             ],
             'data.attributes.name' => 'required|string',
-            'data.attributes.alias' => 'required|string',
+            'data.attributes.alias' => ['required', 'string',
+                Rule::unique('experiments', 'alias')
+                    ->ignore($id ?? null),
+            ],
             'included' => ['array', 'size:2', new SumPercent],
             'included.*.type' => ['required', Rule::in('experiment_branches')],
             'included.*.attributes.name' => 'required|string|distinct',
@@ -30,6 +38,13 @@ class FeatureToggleCreateRequest extends FormRequest
             'included.*.attributes.uid' => 'required|string|distinct',
             'included.0.attributes.percent' => 'required|different:included.1.attributes.percent|in: 0, 100',
             'included.1.attributes.percent' => 'required|different:included.0.attributes.percent|in: 0, 100'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'unique' => 'Experiment uid already exist.',
         ];
     }
 }
